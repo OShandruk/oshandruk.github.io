ef975af0c0685cd0de74b91bff2ae8c1e00f8897
OShandruk <sasha_shandruk1991@ukr.net>
2020-03-10 15:51:16 +0200

    https://github.com/OShandruk/oshandruk.github.io.git
    add burger

---
+++ burger/#source/js/main.js
@@ -0,0 +1,10 @@
+//@prepros-append jq-start.js
+//@prepros-append responsive.js
+//@prepros-append sliders.js
+//@prepros-append scroll.js
+//@prepros-append map.js
+//@prepros-append forms.js
+//@prepros-append jquery.lettering.min.js
+//@prepros-append script.js
+//@prepros-append jq-end.js
+//@prepros-append slick.min.js
